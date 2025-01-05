56a4e951840b2405aa1e446e47f4e31f6711b38a
Sergo <cekuritimafiz@gmail.com>
2025-01-05 15:39:35 +0300

    Update user.go
    Added fullname of Users

--- models/user.go
+++ models/user.go
@@ -2,7 +2,8 @@ package models
 
 // User — структура для хранения данных пользователя.
 type User struct {
+	FullName string   `json:"full_name" bson:"full_name"`
 	Email    string   `json:"email" bson:"email"`
-	Password string   `json:"password" bson:"password"`
+	Password string   `json:"password,omitempty" bson:"password,omitempty"`
 	Roles    []string `json:"roles" bson:"roles"`
 }
